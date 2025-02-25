0ccb38113e88941a1d510a16b3f597c95fb5b36a
Javier-Balestra <javierbalestracoria@gmail.com>
2025-02-25 16:15:25 +0100

    Imagenes no cargaban

--- js/galeria.js
+++ js/galeria.js
@@ -1,11 +1,11 @@
 window.onload = function () {
     const galleryContainer = document.getElementById("gallery");
     const images = [
-        "img/imagen1.jpg",
-        "img/imagen2.jpg",
-        "img/imagen3.jpg",
-        "img/imagen4.jpg",
-        "img/imagen5.jpg"
+        "../img/imagen1.jpg",
+        "../img/imagen2.jpg",
+        "../img/imagen3.jpg",
+        "../img/imagen4.jpg",
+        "../img/imagen5.jpg"
     ];
 
     images.forEach((src, index) => {
